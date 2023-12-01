1522b188f0a9a9c4ad4fab25b5db948ae471b179
Tobias Pflug <tobias.pflug@gmail.com>
2023-12-01 19:13:30 +0100

    day1: in haskell

--- haskell/src/AOC/Day1.hs
+++ haskell/src/AOC/Day1.hs
@@ -1,5 +1,74 @@
-module AOC.Day1 (silly) where
+{-# LANGUAGE ImportQualifiedPost #-}
 
-silly :: String
-silly = "silly"
+module AOC.Day1 (part1, p1, part2, p2, procLine, procLine', findOcurrences) where
 
+import Data.Char qualified as C
+import Data.Functor ((<&>))
+import Data.List qualified as L
+import Data.Maybe qualified as M
+import Data.Monoid
+
+---- part 1
+
+procLine :: String -> Integer
+procLine str = read [go str, go $ reverse str]
+  where
+    go = M.fromJust . L.find C.isNumber
+
+p1 :: String -> Integer
+p1 str = getSum $ foldMap (Sum . procLine) $ lines str
+
+part1 :: String -> IO Integer
+part1 f = readFile f <&> p1
+
+---- part 2
+
+p2 :: String -> Integer
+p2 str = getSum $ foldMap (Sum . procLine') $ lines str
+
+part2 :: String -> IO Integer
+part2 f = readFile f <&> p2
+
+findOcurrences :: String -> String -> [(Int, String)]
+findOcurrences subStr str = go str subStr 0 
+  where
+    go "" _ _ = []
+    go s x n
+      | take (length x) s == x = (n, x) : go (drop (length x) s) x (n + length x)
+      | otherwise = go (tail s) x (n + 1)
+
+procLine' :: String -> Integer
+procLine' str = read [getNum $ head sorted, getNum $ last sorted]
+  where
+    getNum (_, "one") = '1'
+    getNum (_, "two") = '2'
+    getNum (_, "three") = '3'
+    getNum (_, "four") = '4'
+    getNum (_, "five") = '5'
+    getNum (_, "six") = '6'
+    getNum (_, "seven") = '7'
+    getNum (_, "eight") = '8'
+    getNum (_, "nine") = '9'
+    getNum (_,x) = head x
+    indices =
+      concat $ sequence
+        [ findOcurrences "one",
+          findOcurrences "two",
+          findOcurrences "three",
+          findOcurrences "four",
+          findOcurrences "five",
+          findOcurrences "six",
+          findOcurrences "seven",
+          findOcurrences "eight",
+          findOcurrences "nine",
+          findOcurrences "1",
+          findOcurrences "2",
+          findOcurrences "3",
+          findOcurrences "4",
+          findOcurrences "5",
+          findOcurrences "6",
+          findOcurrences "7",
+          findOcurrences "8",
+          findOcurrences "9"
+        ] str
+    sorted = L.sortBy (\(a,_) (b,_) -> compare a b) indices
--- haskell/test/AOC/Day1Spec.hs
+++ haskell/test/AOC/Day1Spec.hs
@@ -3,9 +3,60 @@ module AOC.Day1Spec (spec) where
 import AOC.Day1
 import Test.Hspec
 
+sampleInput :: String
+sampleInput =
+  unlines
+    [ "1abc2",
+      "pqr3stu8vwx",
+      "a1b2c3d4e5f",
+      "treb7uchet"
+    ]
+
+samplePart2 :: String
+samplePart2 =
+  unlines
+    [ "two1nine",
+      "eightwothree",
+      "abcone2threexyz",
+      "xtwone3four",
+      "4nineeightseven2",
+      "zoneight234",
+      "7pqrstsixteen"
+    ]
+
 spec :: Spec
 spec = do
   describe "Day1" $ do
-    describe "exists" $ do
-      it "should exist" $ do
-        silly `shouldBe` "silly"
+    describe "procLine" $ do
+      it "processes a line correctly" $ do
+        procLine (head $ lines sampleInput) `shouldBe` 12
+        procLine (lines sampleInput !! 1) `shouldBe` 38
+        procLine (lines sampleInput !! 2) `shouldBe` 15
+        procLine (lines sampleInput !! 3) `shouldBe` 77
+    describe "p1" $ do
+      it "should yield right result for the sample data" $ do
+        p1 sampleInput `shouldBe` 142
+    describe "part1" $ do
+      it "should solve the puzzle" $ do
+        num <- part1 "./data/day1.txt"
+        num `shouldBe` 54338
+    describe "findOccurrences" $ do
+      it "finds all indices" $ do
+        findOcurrences "two" "two1nine" `shouldBe` [(0,"two")]
+        findOcurrences "3" "three723thxk63" `shouldBe` [(7,"3"), (13,"3")]
+    describe "procLine'" $ do
+      it "extracts the right number" $ do
+        procLine' "two1nine" `shouldBe` 29
+        procLine' "eighttwothree" `shouldBe` 83
+        procLine' "abcone2threexyz" `shouldBe` 13
+        procLine' "xtwone3four" `shouldBe` 24
+        procLine' "4nineeightseven2" `shouldBe` 42
+        procLine' "zoneight234" `shouldBe` 14
+        procLine' "7pqrstsixteen" `shouldBe` 76
+    describe "p2" $ do
+      it "yields correct value for sample input" $ do
+        p2 samplePart2 `shouldBe` 281
+    describe "part2" $ do
+      it "should solve the puzzle" $ do
+        num <- part2 "./data/day1.txt"
+        num `shouldBe` 53389
