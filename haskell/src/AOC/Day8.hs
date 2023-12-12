ef87ce14c04231c539af2cc03abf0bb811151611
Tobias Pflug <tobias.pflug@gmail.com>
2023-12-12 12:30:26 +0100

    day8: yeah okay whatever
    guess there is still a bug for part2, not very enjoyable

---
+++ haskell/src/AOC/Day8.hs
@@ -0,0 +1,154 @@
+module AOC.Day8 where
+
+import AOC.Util
+import Control.Applicative ((<|>))
+import Data.Maybe (fromMaybe)
+
+-- AAA = (BBB, CCC)
+sampleInput :: String
+sampleInput =
+  unlines
+    [ "RL",
+      "",
+      "AAA = (BBB, CCC)",
+      "BBB = (DDD, EEE)",
+      "CCC = (ZZZ, GGG)",
+      "DDD = (DDD, DDD)",
+      "EEE = (EEE, EEE)",
+      "GGG = (GGG, GGG)",
+      "ZZZ = (ZZZ, ZZZ)"
+    ]
+
+sampleInput2 :: String
+sampleInput2 =
+  unlines
+    [ "LLR",
+      "",
+      "AAA = (BBB, BBB)",
+      "BBB = (AAA, ZZZ)",
+      "ZZZ = (ZZZ, ZZZ)"
+    ]
+
+sampleInput3 :: String
+sampleInput3 =
+  unlines
+    [ "LR",
+      "",
+      "11A = (11B, XXX)",
+      "11B = (XXX, 11Z)",
+      "11Z = (11B, XXX)",
+      "22A = (22B, XXX)",
+      "22B = (22C, 22C)",
+      "22C = (22Z, 22Z)",
+      "22Z = (22B, 22B)",
+      "XXX = (XXX, XXX)"
+    ]
+
+
+-- data types
+
+data Dir = R | L
+  deriving (Eq, Show)
+
+type Node = String
+
+type Destination = String
+
+type NodeMap = [(Node, (Node, Node))]
+
+-- parsing
+
+dirs :: Parser [Dir]
+dirs =
+  many
+    ( choice
+        [ L <$ char 'L',
+          R <$ char 'R'
+        ]
+    )
+
+aNode :: Parser String
+aNode = count 3 (letter <|> digit)
+
+connections :: Parser (String, String)
+connections = between (symbol "(") (symbol ")") $ do
+  nodeA <- aNode
+  _ <- symbol ","
+  nodeB <- aNode
+  return (nodeA, nodeB)
+
+nodeInput :: Parser (String, (String, String))
+nodeInput = do
+  node <- aNode
+  _ <- whiteSpace
+  _ <- symbol "="
+  cs <- connections
+  return (node, cs)
+
+puzzleInput :: Parser ([Dir], [(String, (String, String))])
+puzzleInput = do
+  ds <- dirs <* whiteSpace
+  nodes <- sepEndBy1 nodeInput whiteSpace
+  return (ds, nodes)
+
+-- puzzle logic
+
+moveUntilAt :: [Dir] -> Node -> NodeMap -> Integer
+moveUntilAt insts dest nodes = go "AAA" (head insts) 0
+  where
+    go :: Node -> Dir -> Integer -> Integer
+    go curr move n =
+      let (left, right) = fromMaybe (error "Could not find node!") (lookup curr nodes)
+          count = n + 1
+          nextNode = case move of
+            L -> left
+            R -> right
+          instIndex = count `mod` toInteger (length insts)
+          nextInst = insts !! fromInteger instIndex
+       in if nextNode == dest then count else go nextNode nextInst count
+
+getStartNodes :: NodeMap -> [Node]
+getStartNodes nm = fst <$> filter (\(n, _) -> last n == 'A') nm
+
+
+stepUntil :: [Dir] -> [Node] -> NodeMap -> Integer
+stepUntil is startNodes nodeMap = go startNodes (head is) 0
+  where
+    go :: [Node] -> Dir -> Integer -> Integer
+    go nodes move n =
+      let nexts = fmap (\x -> step x move nodeMap) nodes
+          anyFromDest = any (\x -> last x == 'Z') nodes
+          allToDest = all (\x -> last x == 'Z') nexts
+          count = n + 1
+          instIndex = count `mod` toInteger (length is)
+          nextInst = is !! fromInteger instIndex
+       in if not anyFromDest && allToDest then count else go nexts nextInst count
+
+
+step :: Node -> Dir -> NodeMap -> Node
+step node inst nodeMap =
+      let (left, right) = fromMaybe (error "Could not find node!") (lookup node nodeMap)
+       in case inst of
+            L -> left
+            R -> right
+
+
+fuckOff :: [Dir] -> NodeMap -> Integer
+fuckOff is nodeMap = foldl1 lcm $ fmap (\x -> stepUntil is [x] nodeMap) startNodes
+  where
+    startNodes = getStartNodes nodeMap
+
+
+part1 :: IO Integer
+part1 = do
+  inputData <- readFile "./data/day8.txt"
+  return $ case parseString puzzleInput mempty inputData of
+    Success (insts, nodeMap) -> moveUntilAt insts "ZZZ" nodeMap
+    Failure err -> error $ show err
+
+part2 :: IO Integer
+part2 = do
+  inputData <- readFile "./data/day8.txt"
+  return $ case parseString puzzleInput mempty inputData of
+    Success (insts, nodeMap) -> fuckOff insts nodeMap
+    Failure err -> error $ show err
